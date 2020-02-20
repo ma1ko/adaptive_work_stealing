51acc7a81be706dbb516cd75ea586814d1df5263
Maiko Müller <maiko.muller@inria.fr>
2020-02-20 11:53:01 +0100

    Testing new adaptive algorithms on rayon

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,25 @@
 extern crate rayon;
 #[macro_use]
 extern crate lazy_static;
-extern crate crossbeam;
-use crossbeam::channel::*;
+use rayon::send_job;
 use std::option::Option;
 use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::Mutex;
-fn main() {
-    const NUM_THREADS: usize = 4;
+
+fn main() -> Result<(), std::io::Error> {
+    const NUM_THREADS: usize = 20;
     lazy_static! {
         static ref V: [AtomicUsize; NUM_THREADS] = Default::default();
     }
-    lazy_static! {
-        static ref C: [(Sender<JobRef>, Receiver<&'static [u64]>); NUM_THREADS] = [
-            bounded(NUM_THREADS),
-            bounded(NUM_THREADS),
-            bounded(NUM_THREADS),
-            bounded(NUM_THREADS)
-        ];
-    }
 
-    let mut numbers: Mutex<Vec<u64>> = Mutex::new((1..1000000).into_iter().collect());
-    fn steal(x: usize) -> Option<Box<dyn FnOnce() + Send>> {
-        // let steal = |x: usize| -> Option<Box<dyn FnOnce() + Send>> {
-        // println!("Thread {} is stealing", x);
+    let mut numbers: Vec<u64> = (0..100).into_iter().collect();
+    let verify: Vec<u64> = numbers.iter().map(|x| x * 2).collect();
+    fn steal(x: usize) -> Option<()> {
+        if x != 0 {
+            return None;
+        } // Only steal from 0 currently
 
         V[x].fetch_add(1, Ordering::Relaxed);
-        // c[x].receive();
-        // let mut left = do_split_at(numbers, None);
-        // Some(Box::new(|| do_work(Mutex::new(left.to_vec()))))
-        None
+        Some(())
     };
     let pool = rayon::ThreadPoolBuilder::new()
         .num_threads(NUM_THREADS)
@@ -38,26 +28,55 @@ fn main() {
         .unwrap();
 
     pool.install(|| {
-        let split = 10;
-        let mut left = do_split_at(numbers, Some(split));
-        do_stuff_with(&mut left);
-    });
-}
+        let idx: usize = rayon::current_thread_index().unwrap();
+        let mut my_part: &mut [u64] = &mut numbers;
+        loop {
+            let split = 1;
+            if split > my_part.len() {
+                my_part.iter_mut().for_each(|x: &mut u64| *x = *x * 2);
+                break;
+            }
+            let (left, right) = my_part.split_at_mut(split);
+            // do_stuff_with(&mut left);
+            // println!("Main is calculating {}", left[0]);
+            left.iter_mut().for_each(|x: &mut u64| *x = *x * 2);
+            my_part = right;
 
-// split the data in the mutex, put the right part back and return the left
-fn do_split_at(mut numbers: Mutex<Vec<u64>>, index: Option<usize>) -> Box<[u64]> {
-    loop {
-        let mut n = numbers.get_mut().unwrap();
-        let index = index.unwrap_or(n.len() / 2);
-        let (left, right) = n.split_at_mut(index);
-        if left.len() == 0 {
-            return Box::new([]);
+            // check how many other threads need work
+            let ctr: usize = V[idx].swap(0, Ordering::Relaxed);
+            if ctr > 0 {
+                let len = my_part.len();
+                if len < 10 {
+                    // if we don't have much left, it's not worth sending that
+                    // we need some better way to signal other threads there's no work left.
+                    (0..ctr).into_iter().for_each(|_| send_job(Box::new(|| {})));
+                } else {
+                    // keep (1 / ctr), send (ctr - 1) / ctr to the other threads
+                    let (left, right) = my_part.split_at_mut(len / (ctr + 1));
+                    println!(
+                        "{} are waiting, I'm splitting {} to {}",
+                        ctr,
+                        left.len(),
+                        right.len()
+                    );
+                    right.chunks_mut(right.len() / ctr).for_each(|chunk| {
+                        // This move here is important, or this whole thing crashes...
+                        send_job(Box::new(move || {
+                            let idx: usize = rayon::current_thread_index().unwrap();
+                            println!("{}, I'm calculating {} elements", idx, chunk.len());
+                            chunk.iter_mut().for_each(|x: &mut u64| *x = *x * 2);
+                        }));
+                    });
+                    my_part = left; // this threads continues with its part
+                }
+            }
         }
-        let r = &mut right.to_vec();
-        n = r;
-    }
-}
+    });
+    // currently syncronisation doesn't work , so we just wait a bit for everybody to finish
+    let ten_millis = std::time::Duration::from_millis(100);
+    std::thread::sleep(ten_millis);
 
-fn do_stuff_with(numbers: &mut [u64]) {
-    numbers.iter().map(|x| x * 2);
+    assert_eq!(numbers, verify, "wrong output");
+
+    Ok(())
 }
