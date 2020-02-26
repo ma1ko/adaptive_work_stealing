10ae975b2986d9b8087f0ee01324a45cded502b6
Maiko Müller <maiko.muller@inria.fr>
2020-02-26 11:20:44 +0100

    Fix synchronization issues in the recursive join

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ extern crate lazy_static;
 extern crate crossbeam_utils;
 use crossbeam::CachePadded;
 use crossbeam_utils as crossbeam;
+//use rayon_logs as rayon;
 use std::option::Option;
 use std::sync::atomic::{AtomicUsize, Ordering};
-// use rayon_logs as rayon;
 
 pub const NUM_THREADS: usize = 4;
 lazy_static! {
@@ -35,7 +35,7 @@ pub fn steal(backoffs: usize, victim: usize) -> Option<()> {
     None
 }
 pub fn main() -> Result<(), std::io::Error> {
-    let mut numbers: Vec<usize> = (0..100_000).collect();
+    let mut numbers: Vec<usize> = (0..100000).collect();
     let mut verify: Vec<usize> = numbers.clone();
     do_the_work(&mut verify);
     let pool = rayon::ThreadPoolBuilder::new()
@@ -44,9 +44,11 @@ pub fn main() -> Result<(), std::io::Error> {
         .build()
         .unwrap();
 
+    //let (_, log) = pool.logging_install(|| {
     pool.install(|| {
         run(&mut numbers);
     }); // currently syncronisation doesn't work , so we just wait a bit for everybody to finish
+        // log.save_svg("test.svg").expect("failed saving svg");
     assert_eq!(numbers, verify, "wrong output");
     Ok(())
 }
@@ -61,12 +63,6 @@ pub fn run(mut numbers: &mut [usize]) {
         // check how many other threads need work
         let mut steal_counter: usize = V[thread_index].swap(0, Ordering::Relaxed);
         if steal_counter > 0 && len >= MIN_WORK_SIZE {
-            /*
-            println!(
-                "{}: {} steals, {} -> {}",
-                thread_index, steal_counter, numbers[0], len
-            );
-            */
             if steal_counter > NUM_THREADS {
                 // There's more steals than threads, just create tasks for all *other* threads
                 steal_counter = NUM_THREADS - 1
@@ -74,37 +70,38 @@ pub fn run(mut numbers: &mut [usize]) {
             let mut chunks =
                 numbers.chunks_mut(len / (steal_counter + 1/* for me */) + 1 /*round up */);
             numbers = chunks.next().unwrap();
-            fn spawn(chunks: &mut std::slice::ChunksMut<usize>) -> Option<()> {
-                let chunk = chunks.next()?;
-                rayon::join(
-                    || {
-                        spawn(chunks);
-                    },
-                    || {
-                        run(chunk);
-                    },
-                );
-                None
+            fn spawn(chunks: &mut std::slice::ChunksMut<usize>, mut numbers: &mut [usize]) {
+                let chunk = chunks.next();
+                match chunk {
+                    None => {
+                        // finished recursion, let's do our part of the data
+                        run(numbers);
+                    }
+                    Some(chunk) => {
+                        rayon::join(
+                            || {
+                                // prepare another task for the next stealer
+                                spawn(chunks, &mut numbers);
+                            },
+                            || {
+                                // let the stealer process it's part
+                                run(chunk);
+                            },
+                        );
+                    }
+                }
             };
-            spawn(&mut chunks);
-            work_size = MIN_WORK_SIZE;
+            spawn(&mut chunks, &mut numbers);
+            // work_size = MIN_WORK_SIZE;
+            break; // we are finished processing, return the recursion
+        } else {
+            // do *some* work, here: 100 elements
+            let (left, right) = numbers.split_at_mut(std::cmp::min(numbers.len(), work_size));
+            work_size *= 2; // Nobody stole, so we might do more work next time
+
+            do_the_work(left);
+            numbers = right;
         }
-        // do *some* work, here: 100 elements
-        let (left, right) = numbers.split_at_mut(std::cmp::min(numbers.len(), work_size));
-        work_size *= 2;
-
-        /*
-        println!(
-            "{} calculating {} to {}, {} elements",
-            thread_index,
-            left[0],
-            left[left.len() - 1],
-            left.len()
-        );
-        */
-
-        do_the_work(left);
-        numbers = right;
     }
 }
 pub fn work(x: &mut usize) {
@@ -116,11 +113,12 @@ pub fn work(x: &mut usize) {
     }
     */
     // This just creates some static work
-    // *x = (*x as f64).sqrt().sqrt().sqrt().sqrt() as usize;
+    *x = (*x as f64).sqrt().sqrt().sqrt().sqrt() as usize;
     //
     // Try to find a good function to simulate work.
     // I'm choosing N! where N E [0,100], so work varies for each element.
-    *x = (0..*x % 100).into_iter().fold(1, |x, y| (x * y));
+    // *x = (1..*x % 10).into_iter().fold(1, |x, y| (x * y) % 1_000_000);
+    //*x = *x * 2;
 
     // Create an array and sum it up (complex version to stop the optimizer
     // *x = (0..*x / 1000).into_iter().collect::<Vec<usize>>()
